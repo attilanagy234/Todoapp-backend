1038b4008438a7201e4b5379175eb3f82da5ccd9
Attila Nagy <attila.nagy234@gmail.com>
2020-03-06 22:32:17 +0100

    Implement id lookup REST api method

--- src/main/java/com/naatho/todoapp/MainController.java
+++ src/main/java/com/naatho/todoapp/MainController.java
@@ -2,11 +2,9 @@ package com.naatho.todoapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path="/demo")
@@ -29,4 +27,10 @@ public class MainController {
     public @ResponseBody Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
+    @GetMapping(path="/user/{id}")
+    public @ResponseBody
+    Optional<User> getUserByID(@PathVariable Integer id) {
+        return userRepository.findById(id);
+    }
+
 }
