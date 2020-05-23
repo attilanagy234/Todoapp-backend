bbfd778b2a5a85854d34f97ae8ed501cfb294f47
Attila Nagy <attila.nagy234@gmail.com>
2020-05-23 18:51:15 +0200

    add label service

---
+++ src/main/java/com/naatho/todoapp/service/LabelService.java
@@ -0,0 +1,31 @@
+package com.naatho.todoapp.service;
+
+import com.naatho.todoapp.entity.Label;
+import com.naatho.todoapp.repository.LabelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LabelService {
+
+    @Autowired
+    LabelRepository labelRepository;
+
+    public void save(Label label) {
+        labelRepository.save(label);
+    }
+
+    public Iterable<Label> findAll() {
+        return labelRepository.findAll();
+    }
+
+    public Optional<Label> findById(String id) {
+        return labelRepository.findById(id);
+    }
+
+    public void deleteById(String id) {
+        labelRepository.deleteById(id);
+    }
+}
