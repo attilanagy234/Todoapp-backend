14808021fc8625eedec149efeec1cfb94da30403
Attila Nagy <attila.nagy234@gmail.com>
2020-05-23 20:09:02 +0200

    add reminder service

---
+++ src/main/java/com/naatho/todoapp/service/ReminderService.java
@@ -0,0 +1,32 @@
+package com.naatho.todoapp.service;
+
+import com.naatho.todoapp.entity.Project;
+import com.naatho.todoapp.repository.ProjectRepository;
+import com.naatho.todoapp.repository.ReminderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ReminderService {
+
+    @Autowired
+    ReminderRepository reminderRepository;
+
+    public void save(Project project) {
+        reminderRepository.save(project);
+    }
+
+    public Iterable<Project> findAll() {
+        return reminderRepository.findAll();
+    }
+
+    public Optional<Project> findById(Integer id) {
+        return reminderRepository.findById(id);
+    }
+
+    public void deleteById(Integer id) {
+        reminderRepository.deleteById(id);
+    }
+}
