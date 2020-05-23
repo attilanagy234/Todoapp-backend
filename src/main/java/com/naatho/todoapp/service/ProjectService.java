6f39945187c1ccd83e3b18db92ad64de3b88d6bb
Attila Nagy <attila.nagy234@gmail.com>
2020-05-23 18:56:38 +0200

    add project service

---
+++ src/main/java/com/naatho/todoapp/service/ProjectService.java
@@ -0,0 +1,32 @@
+package com.naatho.todoapp.service;
+
+import com.naatho.todoapp.entity.Label;
+import com.naatho.todoapp.entity.Project;
+import com.naatho.todoapp.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    public void save(Project project) {
+        projectRepository.save(project);
+    }
+
+    public Iterable<Project> findAll() {
+        return projectRepository.findAll();
+    }
+
+    public Optional<Project> findById(Integer id) {
+        return projectRepository.findById(id);
+    }
+
+    public void deleteById(Integer id) {
+        projectRepository.deleteById(id);
+    }
+}
