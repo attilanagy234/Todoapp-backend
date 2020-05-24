4c55cbeb16ece049129e3c8754e17c6d5163034a
Atyi Ádám <atyiadam@gmail.com>
2020-05-24 11:06:03 +0200

    add login page

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Todoz");
     w.show();
-    return a.exec();
+    return app.exec();
 }
