22df4a5ffe601b6c74ea97fb101493fb187e16db
Attila Nagy <attila.nagy234@gmail.com>
2020-05-27 00:36:30 +0200

    kafka WIP

--- backend/todo-api/src/main/java/com/naatho/todoapp/utils/ReminderScheduler.java
+++ backend/todo-api/src/main/java/com/naatho/todoapp/utils/ReminderScheduler.java
@@ -5,7 +5,9 @@ import com.naatho.todoapp.service.TaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -21,13 +23,19 @@ public class ReminderScheduler {
 
     Logger logger = LoggerFactory.getLogger(ReminderScheduler.class);
 
+    @Value("${kafka.topic}")
+    private String topic;
+
+    @Autowired
+    private KafkaTemplate<String, Task> kafkaTemplate;
+
     @Transactional
     @Scheduled(fixedRate = 1000)
     public void scheduleTask() {
         List<Task> tasksToRemind = taskService.getTasksToRemind();
         for (Task task : tasksToRemind) {
              logger.info("Found the following tasks to send reminders for name: {} id: {}", task.getName(), task.getId());
-             //TODO: Send to queue
+             kafkaTemplate.send(topic, task);
             task.setReminderTriggered(true);
             task.setReminder(null);
         }
--- backend/todo-common/src/main/java/com/naatho/todoapp/common/Notification.java
+++ backend/todo-common/src/main/java/com/naatho/todoapp/common/Notification.java
@@ -1,4 +1,37 @@
 package com.naatho.todoapp.common;
 
 public class Notification {
+    String name;
+    String email;
+    String text;
+
+    public Notification(String name, String email, String text) {
+        this.name = name;
+        this.email = email;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
