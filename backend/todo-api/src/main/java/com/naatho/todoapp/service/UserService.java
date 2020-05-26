80eba7f350af3eb8e78144a5e513d63657ab471e
Attila Nagy <attila.nagy234@gmail.com>
2020-05-26 11:59:41 +0200

    add more debug and trace level logging in userService

--- backend/todo-api/src/main/java/com/naatho/todoapp/service/UserService.java
+++ backend/todo-api/src/main/java/com/naatho/todoapp/service/UserService.java
@@ -1,10 +1,13 @@
 package com.naatho.todoapp.service;
 
+import com.naatho.todoapp.contoller.MainController;
 import com.naatho.todoapp.entity.Privilege;
 import com.naatho.todoapp.entity.Role;
 import com.naatho.todoapp.entity.User;
 import com.naatho.todoapp.entity.UserDetailsImpl;
 import com.naatho.todoapp.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,40 +26,51 @@ public class UserService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
+    Logger logger = LoggerFactory.getLogger(MainController.class);
+
+
     public void save(User user) {
+        logger.debug("Saving user {}", user);
         userRepository.save(user);
     }
 
 //    @PreAuthorize("hasAuthority('USER_MANAGEMENT')")
     public List<User> findAll() {
+        logger.debug("Querying all users");
         return userRepository.findAll();
     }
 
 //    @PreAuthorize("hasAuthority('USER_MANAGEMENT')")
     public Optional<User> findById(Integer id) {
+        logger.debug("Searching for user by id {} ", id);
         return userRepository.findById(id);
     }
 
 //    @PreAuthorize("hasAuthority('USER_MANAGEMENT')")
     public void deleteById(Integer id) {
+        logger.debug("Attempting to delete user by id {}", id);
         userRepository.deleteById(id);
     }
 
 //    @PreAuthorize("hasAuthority('USER_MANAGEMENT')")
     public User findByEmail(String email) {
+        logger.debug("Searching for user by email {}", email);
         return userRepository.findByEmail(email);
     }
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        logger.debug("Loading user by email...");
         User user = this.findByEmail(email);
         if(user == null) {
             throw new UsernameNotFoundException("The address " + email + " is not valid");
         }
         user.getRoles().size();
-        for(Role role : user.getRoles())
+        for(Role role : user.getRoles()) {
+            logger.trace("Role of user {} is {}", user.getName(), role.getName());
             role.getPrivileges().size();
+        }
         return new UserDetailsImpl(user);
     }
 }
