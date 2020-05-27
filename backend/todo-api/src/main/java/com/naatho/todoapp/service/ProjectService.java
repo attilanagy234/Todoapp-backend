449d13465c4a84177cff0fb03a776286594157fb
Attila Nagy <attila.nagy234@gmail.com>
2020-05-27 04:45:04 +0200

    add email sending

--- backend/todo-api/src/main/java/com/naatho/todoapp/service/ProjectService.java
+++ backend/todo-api/src/main/java/com/naatho/todoapp/service/ProjectService.java
@@ -5,6 +5,7 @@ import com.naatho.todoapp.repository.ProjectRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,21 +19,25 @@ public class ProjectService {
 
     Logger logger = LoggerFactory.getLogger(ProjectService.class);
 
+    @PreAuthorize("hasAuthority('PROJECT_MANAGEMENT')")
     public void save(Project project) {
         logger.debug("Saving project {}", project);
         projectRepository.save(project);
     }
 
+    @PreAuthorize("hasAuthority('PROJECT_MANAGEMENT')")
     public List<Project> findAll() {
         logger.debug("Querying all projects");
         return projectRepository.findAll();
     }
 
+    @PreAuthorize("hasAuthority('PROJECT_MANAGEMENT')")
     public Optional<Project> findById(Integer id) {
         logger.debug("Searching for project by id {}", id);
         return projectRepository.findById(id);
     }
 
+    @PreAuthorize("hasAuthority('PROJECT_MANAGEMENT')")
     public void deleteById(Integer id) {
         logger.debug("Attempting to delete project with id {}", id);
         projectRepository.deleteById(id);
